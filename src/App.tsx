fc3c270629f4125cf3923a95a51d37b52d584792
Fernando <farriagadal94@gmail.com>
2022-02-17 01:48:05 -0300

    test router

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App () {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="favs" element={<Favs />} />
+        <Route path="/favs" element={<Favs />} />
       </Routes>
     </div>
   )
